66d84fe45a8963e6c53b0821983bc043e50c3370
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-04-15 13:05:45 +0300

    Update buttons.tsx

--- client/src/components/card/buttons.tsx
+++ client/src/components/card/buttons.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Buttons = ({ item }: Props) => {
   const client = useQueryClient();
 
-  const { mutate, isPending } = useMutation({x
+  const { mutate, isPending } = useMutation({
     mutationFn: () => api.delete(`/gigs/${item._id}`),
 
     onSuccess: () => {
