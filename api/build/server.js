827111ae1fe3bce2bb488e3d0b1d6ab232b16cc0
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-07-20 00:04:43 +0300

    Update server.js

--- api/build/server.js
+++ api/build/server.js
@@ -2,7 +2,7 @@ import express from "express";s
 const app = express();s
 const port = 3000;s
 app.get("/", (req, res) => {s
-    res.send("Serverdan MerhabalarR");s
+    res.send("Serverdan MerhabalarR");
     
 });
 app.listen(port, () => {
