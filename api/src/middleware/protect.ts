b3e39769a819a0c49eedcd2fc4d6db389d363061
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-08-31 12:17:40 +0300

    Update protect.ts

--- api/src/middleware/protect.ts
+++ api/src/middleware/protect.ts
@@ -2,14 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import error from "../utils/error.ts";
 
-type ExtendedPayload = { id: string; isSeller: boolean } & JwtPayload;
+type ExtendedPayload = { id: string; isSeller: boolean } & JwtPayload;s
 
-const protect = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization?.split(" ")[1] || req.cookies.token;
+const protect = (req: Request, res: Response, next: NextFunction) => {s
+  const token = req.headers.authorization?.split(" ")[1] || req.cookies.token;s
 
-  if (!token) return next(error(403, "Yetkiniz yok (Token bulunamadı)"));
+  if (!token) return next(error(403, "Yetkiniz yok (Token bulunamadı)"));s
 
-  jwt.verify(token, process.env.JWT_KEY as string, (err: any, payload: any) => {
+  jwt.verify(token, process.env.JWT_KEY as string, (err: any, payload: any) => {s
     if (err) return next(error(403, "Tokeniniz geçersiz veya süresi dolmuş"));
 
     req.userId = (payload as ExtendedPayload).id;
