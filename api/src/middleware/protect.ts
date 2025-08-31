d32931a4952de173d98680db93bba8bd125bcbf7
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-08-31 12:17:59 +0300

    Update protect.ts

--- api/src/middleware/protect.ts
+++ api/src/middleware/protect.ts
@@ -7,7 +7,7 @@ type ExtendedPayload = { id: string; isSeller: boolean } & JwtPayload;
 const protect = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1] || req.cookies.token;
 
-  if (!token) return next(error(403, "Yetkiniz yok (Token bulunamadı)"));s
+  if (!token) return next(error(403, "Yetkiniz yok (Token bulunamadı)"));
 
   jwt.verify(token, process.env.JWT_KEY as string, (err: any, payload: any) => {s
     if (err) return next(error(403, "Tokeniniz geçersiz veya süresi dolmuş"));
