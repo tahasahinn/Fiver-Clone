fe0824d3d476c42742e2bb10a379f978cbc12b2e
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-09-09 14:04:49 +0300

    Update index.tsx

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:3000/api",
-  withCredentials: true,
+s
+const api = axios.create({s
+  baseURL: "http://localhost:3000/api",s
+  withCredentials: true,s
 });
 
 export default api;
+s
